c28075dc5df7ad43f1fe918cc1d0c75219bcb555
Wolfgang <wolfgangsmolinsky@gmail.com>
2025-11-21 00:04:34 +0100

    Created I<DomainEntity>Repository for each Domain Entity

---
+++ FlowTrack.Domain/Accounts/IAccountRepository.cs
@@ -0,0 +1,16 @@
+﻿using FlowTrack.Domain.BankConnections;
+using FlowTrack.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowTrack.Domain.Accounts;
+public interface IAccountRepository
+{
+    Task<Account?> GetByIdAsync(AccountId id, CancellationToken cancellationToken = default);
+    Task<Account?> GetByPlaidAccountIdAsync(ExternalId plaidAccountId, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<Account>> GetByConnectionAsync(BankConnectionId bankConnectionId, CancellationToken cancellationToken = default);
+    Task UpsertRangeAsync(IEnumerable<Account> accounts, CancellationToken cancellationToken = default);
+}
---
+++ FlowTrack.Domain/BankConnections/IBankConnectionRepository.cs
@@ -0,0 +1,17 @@
+﻿using FlowTrack.Domain.Shared;
+using FlowTrack.Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowTrack.Domain.BankConnections;
+public interface IBankConnectionRepository
+{
+    Task<BankConnection?> GetByIdAsync(BankConnectionId id, CancellationToken cancellationToken = default);
+    Task<BankConnection?> GetByPlaidItemIdAsync(ExternalId plaidItemId, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<BankConnection>> GetByUserAsync(UserId userId, CancellationToken cancellationToken = default);
+    void Add(BankConnection connection);
+    Task UpdateCursorAsync(BankConnectionId id, string? nextCursor, CancellationToken cancellationToken = default);
+}
