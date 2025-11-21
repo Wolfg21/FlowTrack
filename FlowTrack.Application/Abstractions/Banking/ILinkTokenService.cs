82acdf4f46fdf1786315b9d29d7ebbb29ec3d319
Wolfgang <wolfgangsmolinsky@gmail.com>
2025-11-21 22:05:55 +0100

    Added ILinkTokenService.cs

---
+++ FlowTrack.Application/Abstractions/Banking/ILinkTokenService.cs
@@ -0,0 +1,9 @@
+﻿using FlowTrack.Application.BankConnections.GenerateLinkTokens;
+using FlowTrack.Domain.Users;
+
+namespace FlowTrack.Application.Abstractions.Banking;
+
+public interface ILinkTokenService
+{
+    Task<LinkTokenResponse> GenerateLinkTokenAsync(UserId userId, CancellationToken cancellationToken = default);
+}
