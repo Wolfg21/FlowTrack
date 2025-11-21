a72fc87e707c5ab512be125cd8d379def2b17dc4
Wolfgang <wolfgangsmolinsky@gmail.com>
2025-11-21 21:50:47 +0100

    Added GenerateLinkTokenCommandHandler.cs

---
+++ FlowTrack.Application/BankConnections/GenerateLinkTokens/GenerateLinkTokenCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using FlowTrack.Application.Abstractions.Messaging;
+using FlowTrack.Domain.Abstractions;
+
+namespace FlowTrack.Application.BankConnections.GenerateLinkTokens;
+internal sealed class GenerateLinkTokenCommandHandler : ICommandHandler<GenerateLinkTokenCommand, LinkTokenResponse>
+{
+    public Task<Result<LinkTokenResponse>> Handle(GenerateLinkTokenCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
