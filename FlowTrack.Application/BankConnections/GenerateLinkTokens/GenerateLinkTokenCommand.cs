29a9c1f1391763e3f78c52d6c133d66299e49587
Wolfgang <wolfgangsmolinsky@gmail.com>
2025-11-21 21:50:18 +0100

    Added GeneratelinkTokenCommand

---
+++ FlowTrack.Application/BankConnections/GenerateLinkTokens/GenerateLinkTokenCommand.cs
@@ -0,0 +1,4 @@
+﻿using FlowTrack.Application.Abstractions.Messaging;
+
+namespace FlowTrack.Application.BankConnections.GenerateLinkTokens;
+public record GenerateLinkTokenCommand(Guid UserId) : ICommand<LinkTokenResponse>;
