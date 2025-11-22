8d62286db37676a2e05ea301f2f51c32855d4c2e
Wolfgang <wolfgangsmolinsky@gmail.com>
2025-11-22 14:24:40 +0100

    Added UpsertInstitutionCommand.cs

---
+++ FlowTrack.Application/Institutions/UpsertInstitutions/UpsertInstitutionCommand.cs
@@ -0,0 +1,18 @@
+﻿using FlowTrack.Application.Abstractions.Messaging;
+using FlowTrack.Domain.Institutions;
+using FlowTrack.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowTrack.Application.Institutions.UpsertInstitutions;
+public sealed record UpsertInstitutionCommand(
+    string PlaidInsitutionId,
+    string Name,
+    List<string> CountryCodes,
+    List<string> Products,
+    string? PrimaryColorHex,
+    string? Logo,
+    string? Url) : ICommand<Guid>;
